f49f2ce497088c07b0243d2a5d0d0c833d06bed0
Felizj17 <felizj17@gmail.com>
2023-04-18 14:58:00 -0400

    make put and delete routes

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -2,18 +2,38 @@ const transactions = require('express').Router()
 const bodyParser = require('body-parser')
 const transactionsArray = require('../models/transactions')
 
+const jsonParser = bodyParser.json()
+
 transactions.get('/', (req, res) => {
   res.json(transactionsArray)
 })
 
-transactions.get('/:id', (req,res)=>{
-    const id = req.params
-    res.status(200).json(transactionsArray[id])
-})
-transactions.post('/new',(req,res)=>{
-    const newTransaction = req.body
-    transactionsArray.push(newTransaction)
-    res.status(202).json(transactionsArray) 
+transactions.get('/:index', (req, res) => {
+  const id = req.params.index
+  res.status(200).json(transactionsArray[id])
 })
 
+transactions.post('/new', jsonParser, (req, res) => {
+  const newTransaction = req.body
+  transactionsArray.push(newTransaction)
+  res.status(202).json(transactionsArray)
+})
+
+transactions.put('/:index/edit', jsonParser, (req, res) => {
+  if (transactionsArray[req.params.index]) {
+    transactionsArray[req.params.index] = req.body
+    res.status(200).json(transactionsArray[req.params.index])
+  } else {
+    res.status(404).json({ error: 'Not Found' })
+  }
+})
+
+transactions.delete('/index/delete', (req, res) => {
+  if (transactionsArray[req.params.index]) {
+    const deletedTransaction = transactionsArray.splice(req.params.index, 1)
+    res.status(200).json(deletedTransaction)
+  }else{
+    res.status(404).json({error:'Not Found'})
+  }
+})
 module.exports = transactions
