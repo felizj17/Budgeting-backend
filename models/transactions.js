30ffaf7bacf9e3168d51a54f2829d0b92f5beb0e
Felizj17 <felizj17@gmail.com>
2023-04-19 16:29:19 -0400

    edits to the models

--- models/transactions.js
+++ models/transactions.js
@@ -1,18 +1,50 @@
 module.exports = [
     {
-        id:123,
+        id:'f642debe-b43a-49d1-baf8-7b604f5d4463',
         item_name:'food',
-        amount:200,
-        date:new Date(),
+        amount:-200,
+        date:'2023-01-24',
         from:'key food',
         category:'grocery',   
     },
     {
-        id:126,
-        item_name:'food',
-        amount:20,
-        date:new Date(),
-        from:'key food',
-        category:'grocery',   
-    }
+        id: "6642debe-b43a-49d1-baf8-7b604f5d4463",
+        item_name: "Income",
+        amount: 2524.45,
+        date: "2023-04-19",
+        from: "Direct Deposit",
+        category: "deposit"
+      },
+      {
+        id: "38dba6c8-4de6-451e-9847-8cc20a61d4f6",
+        item_name: "Coffee",
+        amount: -3.68,
+        date: "2023-04-19",
+        from: "Dunkin Donuts",
+        category: "food"
+      },
+      {
+        id: "bc98fad0-9cce-4be5-9159-c17ce6220054",
+        item_name: "Dinner",
+        amount: -42.15,
+        date: "2023-04-19T00:00:00.000Z",
+        from: "Peter Lugers",
+        category: "food"
+      },
+      {
+        id: "ea730a8a-66c4-4e7f-b9ce-e417e67e4da0",
+        item_name: "Television",
+        amount: -450.82,
+        date: "2023-04-19T00:00:00.000Z",
+        from: "Best buy",
+        category: "entertainment"
+      },
+      {
+        id: "aad5b22c-86d4-422e-b3cb-8c7a4b5ee769",
+        item_name: "zelle payment for pool",
+        amount: 22.00,
+        date: "2023-04-19T00:00:00.000Z",
+        from: "Diego",
+        category: "entertainment"
+      }
 ]
