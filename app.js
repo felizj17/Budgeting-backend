bc9d356291a2869cc56532c3e97a86257dd4e1dc
Felizj17 <felizj17@gmail.com>
2023-04-19 15:18:18 -0400

    added cors changed edit and delete to use id instead of index

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const app = require('express')()
 const cors = require('cors')
 const transactionController = require('./controllers/transactionController')
 
+app.use(cors())
 app.use('/transactions', transactionController)
 app.get('/', (_,res)=>{
     res.send('Budgeting app')
--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -14,26 +14,37 @@ transactions.get('/:index', (req, res) => {
 })
 
 transactions.post('/new', jsonParser, (req, res) => {
-  const newTransaction = req.body
+  const newTransaction = {
+    id: req.body.id,
+    item_name: req.body.item_name,
+    amount: parseFloat(req.body.amount).toFixed(2),
+    date: new Date(req.body.date),
+    from: req.body.from,
+    category: req.body.category,
+  }
   transactionsArray.push(newTransaction)
   res.status(202).json(transactionsArray)
 })
 
-transactions.put('/:index/edit', jsonParser, (req, res) => {
-  if (transactionsArray[req.params.index]) {
-    transactionsArray[req.params.index] = req.body
-    res.status(200).json(transactionsArray[req.params.index])
+transactions.put('/:id/edit', jsonParser, (req, res) => {
+  const id = req.params.id 
+  const curTransaction = transactionsArray.filter(transaction=>transaction.id.toString()===id)[0]
+  if (curTransaction) {
+    transactionsArray[transactionsArray.indexOf(curTransaction)] = req.body
+    res.status(200).json(transactionsArray[transactionsArray.indexOf(curTransaction)])
   } else {
     res.status(404).json({ error: 'Not Found' })
   }
 })
 
-transactions.delete('/index/delete', (req, res) => {
-  if (transactionsArray[req.params.index]) {
-    const deletedTransaction = transactionsArray.splice(req.params.index, 1)
+transactions.delete('/:id/delete', (req, res) => {
+  const id = req.params.id
+  const curTransaction = transactionsArray.filter(transaction=>transaction.id.toString()===id)[0]
+  if (curTransaction) {
+    const deletedTransaction = transactionsArray.splice(transactionsArray.indexOf(curTransaction), 1)
     res.status(200).json(deletedTransaction)
-  }else{
-    res.status(404).json({error:'Not Found'})
+  } else {
+    res.status(404).json({ error: 'Not Found' })
   }
 })
 module.exports = transactions
